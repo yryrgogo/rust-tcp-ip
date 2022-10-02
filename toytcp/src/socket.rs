fd712a7cd2466fa5853db1ff45797e33eec34bea
yryrgogo <yryrgogo@gmail.com>
2022-10-02 11:36:42 +0900

    :+1: update toytcp sources

--- toytcp/src/socket.rs
+++ toytcp/src/socket.rs
@@ -11,7 +11,7 @@ use std::time::SystemTime;
 
 const SOCKET_BUFFER_SIZE: usize = 4380;
 
-/// (local_addr, remote_addr, local_port, remote_port) のタプルでソケットを識別する
+/// (local_addr, remote_addr, local_port, remote_port) のタプルでソケットを識別
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
 pub struct SockID(pub Ipv4Addr, pub Ipv4Addr, pub u16, pub u16);
 
@@ -20,11 +20,10 @@ pub struct Socket {
     pub remote_addr: Ipv4Addr,
     pub local_port: u16,
     pub remote_port: u16,
-    pub send_param: SendParam,
-    pub recv_param: RecvParam,
-    pub status: TcpStatus,
     pub sender: TransportSender,
 }
+
+pub enum TcpStatus {}
 
 impl Socket {
     pub fn new(
@@ -32,7 +31,6 @@ impl Socket {
         remote_addr: Ipv4Addr,
         local_port: u16,
         remote_port: u16,
-        status: TcpStatus,
     ) -> Result<Self> {
         let (sender, _) = transport::transport_channel(
             65535,
@@ -43,53 +41,19 @@ impl Socket {
             remote_addr,
             local_port,
             remote_port,
-            send_param: SendParam {
-                unacked_seq: 0,
-                initial_seq: 0,
-                next: 0,
-                window: SOCKET_BUFFER_SIZE as u16,
-            },
-            recv_param: RecvParam {
-                initial_seq: 0,
-                next: 0,
-                window: SOCKET_BUFFER_SIZE as u16,
-                tail: 0,
-            },
-            status: status,
             sender,
         })
     }
 
-    pub fn send_tcp_packet(
-        &mut self,
-        seq: u32,
-        ack: u32,
-        flag: u8,
-        payload: &[u8],
-    ) -> Result<usize> {
+    pub fn send_tcp_packet(&mut self, flag: u8, payload: &[u8]) -> Result<usize> {
         let mut tcp_packet = TCPPacket::new(payload.len());
         tcp_packet.set_src(self.local_port);
         tcp_packet.set_dest(self.remote_port);
-        tcp_packet.set_seq(seq);
-        tcp_packet.set_ack(ack);
-        tcp_packet.set_data_offset(5); // toytcp ではオプションフィールドを使わないため固定
         tcp_packet.set_flag(flag);
-        tcp_packet.set_window_size(self.recv_param.window);
-        tcp_packet.set_payload(payload);
-        tcp_packet.set_checksum(util::ipv4_checksum(
-            &tcp_packet.packet(),
-            8,
-            &[],
-            &self.local_addr,
-            &self.remote_addr,
-            IpNextHeaderProtocols::Tcp,
-        ));
         let sent_size = self
             .sender
             .send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr))
-            .context(format!("failed to send: \n{:?}", tcp_packet))?;
-
-        dbg!("sent", &tcp_packet);
+            .unwrap();
         Ok(sent_size)
     }
 
@@ -100,50 +64,5 @@ impl Socket {
             self.local_port,
             self.remote_port,
         )
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct SendParam {
-    pub unacked_seq: u32, // 送信後まだ ack されていない seq の先頭
-    pub next: u32,        // 次の送信
-    pub window: u16,      // 送信ウィンドウサイズ
-    pub initial_seq: u32, // 初期送信 seq
-}
-
-#[derive(Clone, Debug)]
-pub struct RecvParam {
-    pub next: u32,        // 次受診する seq
-    pub window: u16,      // 受診ウィンドウ
-    pub initial_seq: u32, // 初期受信 seq
-    pub tail: u32,        // 受信 seq の最後尾
-}
-
-#[derive(PartialEq, Eq, Debug, Clone)]
-pub enum TcpStatus {
-    Listen,
-    SynSent,
-    SynRcvd,
-    Established,
-    FinWait1,
-    FinWait2,
-    TimeWait,
-    CloseWait,
-    LastAck,
-}
-
-impl Display for TcpStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            TcpStatus::Listen => write!(f, "LISTEN"),
-            TcpStatus::SynSent => write!(f, "SYNSENT"),
-            TcpStatus::SynRcvd => write!(f, "SYNRCVD"),
-            TcpStatus::Established => write!(f, "ESTABLISHED"),
-            TcpStatus::FinWait1 => write!(f, "FINWAIT1"),
-            TcpStatus::FinWait2 => write!(f, "FINWAIT2"),
-            TcpStatus::TimeWait => write!(f, "TIMEWAIT"),
-            TcpStatus::CloseWait => write!(f, "CLOSEWAIT"),
-            TcpStatus::LastAck => write!(f, "LASTACK"),
-        }
     }
 }
--- toytcp/src/tcp.rs
+++ toytcp/src/tcp.rs
@@ -20,37 +20,20 @@ const MSS: usize = 1460;
 const PORT_RANGE: Range<u16> = 40000..60000;
 
 pub struct TCP {
-    sockets: RwLock<HashMap<SockID, Socket>>,
-    event_condvar: (Mutex<Option<TCPEvent>>, Condvar),
+    sockets: HashMap<SockID, Socket>,
 }
 
 impl TCP {
-    pub fn new() -> Arc<Self> {
-        let sockets = RwLock::new(HashMap::new());
-        let tcp = Arc::new(Self {
-            sockets,
-            event_condvar: (Mutex::new(None), Condvar::new()),
-        });
-        let cloned_tcp = tcp.clone();
-        std::thread::spawn(move || {
-            // パケットの受信用スレッド
-            cloned_tcp.receive_handler().unwrap();
-        });
+    pub fn new() -> Self {
+        let sockets = HashMap::new();
+        let tcp = Self { sockets };
         tcp
     }
 
     fn select_unused_port(&self, rng: &mut ThreadRng) -> Result<u16> {
-        for _ in 0..(PORT_RANGE.end - PORT_RANGE.start) {
-            let local_port = rng.gen_range(PORT_RANGE);
-            let table = self.sockets.read().unwrap();
-            if table.keys().all(|k| local_port != k.2) {
-                return Ok(local_port);
-            }
-        }
-        anyhow::bail!("no available port found.");
+        Ok(33445)
     }
 
-    /// ターゲットに接続し、接続済みソケットの ID を返す
     pub fn connect(&self, addr: Ipv4Addr, port: u16) -> Result<SockID> {
         let mut rng = rand::thread_rng();
         let mut socket = Socket::new(
@@ -58,189 +41,13 @@ impl TCP {
             addr,
             self.select_unused_port(&mut rng)?,
             port,
-            TcpStatus::SynSent,
         )?;
-        socket.send_param.initial_seq = rng.gen_range(1..1 << 31);
-        socket.send_tcp_packet(socket.send_param.initial_seq, 0, tcpflags::SYN, &[])?;
-        socket.send_param.next = socket.send_param.initial_seq + 1;
-        let mut table = self.sockets.write().unwrap();
+        socket.send_tcp_packet(tcpflags::SYN, &[])?;
         let sock_id = socket.get_sock_id();
-
-        table.insert(sock_id, socket);
-
-        // ロックを外してイベントの待機。受信スレッドがロックを取得できるようにするため
-        drop(table);
-        self.wait_event(sock_id, TCPEventKind::ConnectionCompleted);
-
         Ok(sock_id)
     }
-
-    /// 受信スレッド用のメソッド
-    fn receive_handler(&self) -> Result<()> {
-        dbg!("begin recv thread");
-        let (_, mut receiver) = transport::transport_channel(
-            65535,
-            TransportChannelType::Layer3(IpNextHeaderProtocols::Tcp), // IP Address が必要なので IP パケットレベルで取得
-        )
-        .unwrap();
-        let mut packet_iter = transport::ipv4_packet_iter(&mut receiver);
-
-        loop {
-            let (packet, remote_addr) = match packet_iter.next() {
-                Ok((p, r)) => (p, r),
-                Err(_) => continue,
-            };
-            let local_addr = packet.get_destination();
-
-            // pnet の TcpPacket を生成
-            let tcp_packet = match TcpPacket::new(packet.payload()) {
-                Some(p) => p,
-                None => {
-                    continue;
-                }
-            };
-            // pnet の TcpPacket から tcp::TCPPacket に変換する
-            let packet = TCPPacket::from(tcp_packet);
-
-            let remote_addr = match remote_addr {
-                IpAddr::V4(addr) => addr,
-                _ => {
-                    continue;
-                }
-            };
-
-            let mut table = self.sockets.write().unwrap();
-            let socket = match table.get_mut(&SockID(
-                local_addr,
-                remote_addr,
-                packet.get_dest(),
-                packet.get_src(),
-            )) {
-                Some(socket) => socket, // 接続済みソケット
-                None => match table.get_mut(&SockID(
-                    local_addr,
-                    UNDETERMINED_IP_ADDR,
-                    packet.get_dest(),
-                    UNDETERMINED_PORT,
-                )) {
-                    Some(socket) => socket,
-                    None => continue,
-                },
-            };
-            if !packet.is_correct_checksum(local_addr, remote_addr) {
-                dbg!("invalid checksum");
-                continue;
-            }
-            let sock_id = socket.get_sock_id();
-            if let Err(error) = match socket.status {
-                TcpStatus::SynSent => self.synsent_handler(socket, &packet),
-                _ => {
-                    dbg!("not implemented state");
-                    Ok(())
-                }
-            } {
-                dbg!(error);
-            }
-        }
-    }
-
-    fn synsent_handler(&self, socket: &mut Socket, packet: &TCPPacket) -> Result<()> {
-        dbg!("synsent handler");
-        if packet.get_flag() & tcpflags::ACK > 0
-            && socket.send_param.unacked_seq <= packet.get_ack()
-            && packet.get_ack() <= socket.send_param.next
-            && packet.get_flag() & tcpflags::SYN > 0
-        {
-            socket.recv_param.next = packet.get_seq() + 1;
-            socket.recv_param.initial_seq = packet.get_seq();
-            socket.send_param.unacked_seq = packet.get_ack();
-            socket.send_param.window = packet.get_window_size();
-
-            if socket.send_param.unacked_seq > socket.send_param.initial_seq {
-                socket.status = TcpStatus::Established;
-                socket.send_tcp_packet(
-                    socket.send_param.next,
-                    socket.recv_param.next,
-                    tcpflags::ACK,
-                    &[],
-                )?;
-                dbg!("status: synsent ->", &socket.status);
-                self.publish_event(socket.get_sock_id(), TCPEventKind::ConnectionCompleted);
-            } else {
-                socket.status = TcpStatus::SynRcvd;
-                socket.send_tcp_packet(
-                    socket.send_param.next,
-                    socket.recv_param.next,
-                    tcpflags::ACK,
-                    &[],
-                )?;
-                dbg!("status: synsent ->", &socket.status);
-            }
-        }
-        Ok(())
-    }
-
-    /// 指定した Socket ID と種別のイベントを待機
-    fn wait_event(&self, sock_id: SockID, kind: TCPEventKind) {
-        let (lock, cvar) = &self.event_condvar;
-        let mut event = lock.lock().unwrap();
-        loop {
-            if let Some(ref e) = *event {
-                if e.sock_id == sock_id && e.kind == kind {
-                    break;
-                }
-            }
-            // cvar が notify されるまで event のロックを外して待機
-            event = cvar.wait(event).unwrap();
-        }
-        dbg!(&event);
-        *event = None;
-    }
-
-    /// 指定の Socket ID にイベントを発行する
-    fn publish_event(&self, sock_id: SockID, kind: TCPEventKind) {
-        let (lock, cvar) = &self.event_condvar;
-        let mut event = lock.lock().unwrap();
-        *event = Some(TCPEvent::new(sock_id, kind));
-        cvar.notify_all();
-    }
 }
 
-/// 宛先 IP アドレスに対する送信元インタフェースの IP アドレスを取得する
-/// iproute2-ssl80129 で動作を確認。バージョンによって挙動が変わるかも
 fn get_source_addr_to(addr: Ipv4Addr) -> Result<Ipv4Addr> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!("ip route get {} | grep src", addr))
-        .output()?;
-    let mut output = str::from_utf8(&output.stdout)?
-        .trim()
-        .split_ascii_whitespace();
-    while let Some(s) = output.next() {
-        if s == "src" {
-            break;
-        }
-    }
-    let ip = output.next().context("failed to get src ip")?;
-    ip.parse().context("failed to parse source ip")
-}
-
-#[derive(Debug, Clone, PartialEq)]
-struct TCPEvent {
-    sock_id: SockID, // イベント発生元の Socket ID
-    kind: TCPEventKind,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum TCPEventKind {
-    ConnectionCompleted,
-    Acked,
-    DataArrived,
-    ConnectionClosed,
-}
-
-impl TCPEvent {
-    fn new(sock_id: SockID, kind: TCPEventKind) -> Self {
-        Self { sock_id, kind }
-    }
+    Ok("10.0.0.1".parse().unwrap())
 }
