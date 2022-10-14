2649faf851aca9fb80b24332c0fad9b0b8e44cba
Go Horikoshi <yryrgogo@gmail.com>
2022-10-14 21:56:50 +0000

    :+1: start tcp/udp communication

---
+++ network/tcp_udp/src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port].");
+        std::process::exit(1);
+    }
+    let protocol = &args[1];
+}
