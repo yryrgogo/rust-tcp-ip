2ad2dbfacb455811ea00a2f677da9dc3803b4396
yryrgogo <yryrgogo@gmail.com>
2022-10-16 22:11:40 +0900

    :+1: add tcp client & server

--- network/tcp_udp/src/tcp_client.rs
+++ network/tcp_udp/src/tcp_client.rs
@@ -1,1 +1,18 @@
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::TcpStream;
+use std::str;
 
+pub fn connect(address: &str) -> io::Result<()> {
+    let mut stream = TcpStream::connect(address)?;
+    loop {
+        let mut input = String::new();
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        handle.read_line(&mut input)?;
+        stream.write_all(input.as_bytes())?;
+        let mut reader = BufReader::new(&stream);
+        let mut buffer = Vec::new();
+        reader.read_until(b'\n', &mut buffer)?;
+        print!("{}", str::from_utf8(&buffer).unwrap());
+    }
+}
--- network/tcp_udp/src/tcp_server.rs
+++ network/tcp_udp/src/tcp_server.rs
@@ -1,1 +1,26 @@
+use anyhow::{Context as _, Result};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{str, thread};
 
+pub fn serve(address: &str) -> Result<()> {
+    let listener = TcpListener::bind(address)?;
+    loop {
+        let (stream, _) = listener.accept()?;
+        thread::spawn(move || handler(stream).context("failed to handle stream"));
+    }
+}
+
+fn handler(mut stream: TcpStream) -> Result<()> {
+    debug!("Handling data from {}", stream.peer_addr()?);
+    let mut buffer = [0u8; 1024];
+    loop {
+        let nbytes = stream.read(&mut buffer)?;
+        if nbytes == 0 {
+            debug!("Connection closed");
+            return Ok(());
+        }
+        print!("{}", str::from_utf8(&buffer[..nbytes])?);
+        stream.write_all(&buffer[..nbytes])?;
+    }
+}
