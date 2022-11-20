bdec30541bc7199c0f5f18a33dab1a512c464edc
yryrgogo <yryrgogo@gmail.com>
2022-11-20 06:52:00 +0000

    :+1: add ip netns script

--- router/netns/create_initial_vnet.sh
+++ router/netns/create_initial_vnet.sh
@@ -25,25 +25,26 @@ ip netns exec host1 ip link set host1-router1 up
 ip netns exec host1 ethtool -K host1-router1 rx off tx off
 ip netns exec host1 ip route add default via 192.168.1.1
 
+ip netns exec router1 ip addr add 192.168.1.1/24 dev router1-host1
+ip netns exec router1 ip addr add 192.168.0.1/24 dev router1-router2
 ip netns exec router1 ip link set router1-host1 up
 ip netns exec router1 ethtool -K router1-host1 rx off tx off
 ip netns exec router1 ip link set router1-router2 up
 ip netns exec router1 ethtool -K router1-router2 rx off tx off
-# ip netns exec router1 ip addr add 192.168.1.1/24 dev router1-host1
-# ip netns exec router1 ip addr add 192.168.0.1/24 dev router1-router2
-# ip netns exec router1 ip route add default via 192.168.0.2
-# ip netns exec router1 sysctl -w net.ipv4.ip_forward=1
+ip netns exec router1 ip route add default via 192.168.0.2
+ip netns exec router1 sysctl -w net.ipv4.ip_forward=1
 
+ip netns exec router2 ip addr add 192.168.0.2/24 dev router2-router1
+ip netns exec router2 ip addr add 192.168.2.1/24 dev router2-host2
 ip netns exec router2 ip link set router2-router1 up
 ip netns exec router2 ethtool -K router2-router1 rx off tx off
 ip netns exec router2 ip link set router2-host2 up
 ip netns exec router2 ethtool -K router2-host2 rx off tx off
-# ip netns exec router2 ip addr add 192.168.0.2/24 dev router2-router1
-# ip netns exec router2 ip route add default via 192.168.0.1
-# ip netns exec router2 ip addr add 192.168.2.1/24 dev router2-host2
-# ip netns exec router2 sysctl -w net.ipv4.ip_forward=1
+ip netns exec router2 ip route add default via 192.168.0.1
+ip netns exec router2 sysctl -w net.ipv4.ip_forward=1
 
 ip netns exec host2 ip addr add 192.168.2.2/24 dev host2-router2
 ip netns exec host2 ip link set host2-router2 up
 ip netns exec host2 ethtool -K host2-router2 rx off tx off
 ip netns exec host2 ip route add default via 192.168.2.1
+
